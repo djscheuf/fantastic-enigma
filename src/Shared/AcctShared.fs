1b2ebc4d3a85c7de6d4b25712c2276c5185bfa7a
Daniel Scheufler <djscheufler@gmail.com>
2021-09-12 11:52:41 -0500

    wip(accounting event source)

---
+++ src/Shared/AcctShared.fs
@@ -0,0 +1,35 @@
+namespace AcctShared
+
+open System
+open Events
+
+module Account =
+    let CreditAcctEventKind = Events.createKind("Credit");
+
+    let DebitAcctEventKind = Events.createKind("Debit");
+
+    type CreditAcctEventDetails = { Amount: float; }
+    type DebitAcctEventDetails = { Amount: float; }
+
+    let deposit (amount: float) = 
+        let eventDetails = { Amount = amount};
+        {
+            Id = Guid.NewGuid()
+            Kind = CreditAcctEventKind
+            Details = eventDetails.ToString()
+        }
+
+    let credit(amount: float) =
+        let eventDetails = {Amount = amount};
+        {
+            Id = Guid.NewGuid()
+            Kind = DebitAcctEventKind
+            Details = eventDetails.ToString()
+        }
+
+type IAccountApi =
+    { 
+        getBalance: unit -> Async<float>
+        deposit: float -> Async<Result<unit,string>> 
+        withdraw: float -> Async<Result<unit,string>> 
+    }
---
+++ src/Shared/Events.fs
@@ -0,0 +1,16 @@
+namespace Events
+
+open System
+
+
+type EventKind = {Id: Guid; Name: string}
+
+type Event = {Id: Guid; Kind: EventKind; Details: string ;} 
+
+module Events = 
+    let createKind(name: string) = 
+        {
+            Id = Guid.NewGuid()
+            Name = name 
+        }
+
